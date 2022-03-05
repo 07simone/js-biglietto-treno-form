c60ee4df5c56ab63720ed82a8620c73aaf7524bc
simone andreozzi <simoneandreozzi8@gmail.com>
2022-03-05 14:01:35 +0100

    treno-complex

--- js/main.js
+++ js/main.js
@@ -1,70 +1,49 @@
-/* const kmDaPercorrere = parseInt(x,base); */
+    let userName = document.getElementById("nome");
+    let userAge = document.querySelector("#eta");
+    let userDistance = document.querySelector("#numeri-km");
+    const prezzoKm = 0.27;
 
-/* const prezzokm = 0.27;
-let prezzo = prezzokm * kmDaPercorrere; */
-/* 
- if( etaUtente < 18){
+    const generatorTicket = document.getElementById("button-generator");
+
+
+    generatorTicket.addEventListener("click",function(){
     
-    prezzoFinale =product- (product * 20 / 100) ;
-    document.getElementById('km').innerHTML = prezzoFinale .toFixed(2) ;
+        let prezzoBiglietto = prezzoKm * userDistance.value;
+        console.log(prezzoBiglietto);
 
-} else if (etaUtente >= 18 && etaUtente <= 65) { 
+        console.log(userName.value);
+        console.log(userAge.value);
 
-    product = (0.21 * kmUtente) .toPrecision(2);
-    document.getElementById('output').innerHTML = product .toFixed(2) ;
+        if (userAge.value == "minorenne"){
+            prezzoBiglietto -= prezzoBiglietto * 0.17;
+            console.log ("Scontato del 17%");
+        } else if ( userAge.value == "over65"){
+            prezzoBiglietto -= prezzoBiglietto *0.33;
+            console.log ("Scontato del 33%");
+        } else{
+                console.log("nessuno sconto");
+            }
+            console.log(prezzoBiglietto);
 
-} else if( etaUtente >65){
 
-    prezzoFinale2 = product - (product * 40 / 100) ;
-    
-    document.getElementById('km-over').innerHTML = prezzoFinale2 .toFixed(2) ;
-}
-  */
+            let output = document.getElementById("passegero")
+            output.innerHTML = (`${userName.value}`);
+
+            let outputcost = document.getElementById("totale")
+            outputcost.innerHTML = (`${prezzoBiglietto.toFixed(2)} €`);
+        }
+    )
 
 
-/* let message =""; */
+    const ticketReset = document.getElementById("button-reset");
+    ticketReset.addEventListener("click", function(){
+        let prezzoBiglietto = prezzoKm * userDistance.value;
+        userName.value ="";
+        userAge.value ="";
+        userDistance.value ="";
+        let output = document.getElementById("passegero")
+        output.innerHTML = (`${userName.value}`);
 
-document.querySelector(".btn-secondary").addEventListener('click', function(){
-     let messaggioOfferta ="";
-     let etaUtente = document.getElementById("inputGroupSelect03");
-    console.log(etaUtente.value);
-
-    const newName = document.getElementById("nome");
-    console.log(newName);
-    /* const randomInt = Math.floor(Math.random() * 9) +1; */
-    
-    console.log(`${newName.value }`);       
-    document.getElementById("passegero").innerHTML = (`${newName.value }`);
-    
-
-    document.getElementById("offering").innerHTML = (`${messaggioOfferta}`); 
-      console.log(offering);  
-    
-    let minorenne="";
-    let maggiorenne="";
-    let over65="";
-
-    if( etaUtente == minorenne ){
-    
-        prezzoFinale =product- (product * 0.17) ;
-        document.getElementById('km').innerHTML = prezzoFinale .toFixed(2) ;
-        messaggioOfferta ="il prezzo del biglieto sarà scontato del 17%";
-        console.log(messaggioOfferta);
-    
-    } else if (etaUtente == minorenne && etaUtente == maggiorenne) { 
-    
-        product = (0.21 * kmUtente) .toPrecision(2);
-        document.getElementById('output').innerHTML = product .toFixed(2) ;
-        messaggioOfferta ="il prezzo del biglieto non verrà scontato"
-
-    } else if( etaUtente == over65){
-    
-        prezzoFinale2 = product - (product * 0.33) ;
-        document.getElementById('km-over').innerHTML = prezzoFinale2 .toFixed(2) ;
-        messaggioOfferta ="il prezzo del biglieto verrà scontato del 33%"
-    }  
-    
-
-    /* document.getElementById("carrozza").innerHTML = (`${randomInt.value }`); */
-    /* console.log(`${randomInt.value }`) */
-});
+        let outputcost = document.getElementById("totale")
+        outputcost.innerHTML = (`${prezzoBiglietto.toFixed(2)} €`);
+    });
